eef7e47412d634f3cb2da8fecf4a1eeb83e1d38f
Hay Kranen <huskyr@gmail.com>
2014-02-13 11:50:53 +0100

    KB Permalink now does permalinks to ALL OCLC PICA catalogues

--- public_html/kbpermalink/index.php
+++ public_html/kbpermalink/index.php
@@ -2,16 +2,16 @@
     require '../../lib/class-hay.php';
     Hay::header();
 ?>
-        <h1>KB Permalink</h1>
-        <h2>A small Javascript bookmarklet to generate a permalink to <a href="http://www.kb.nl">Dutch National Library</a> catalogue records.</h2>
+        <h1>Pica Permalink</h1>
+        <h2>A small Javascript bookmarklet to generate a permalink to library catalogues that use the <a href="https://en.wikipedia.org/wiki/OCLC_PICA">OCLC PICA</a> software</h2>
 
-        <p>The <a href="http://opc4.kb.nl/">catalogue of the Koninklijke Bibliotheek</a> (KB) is not that good, and there's no easy way from the interface to generate a permalink to a record. When browsing search results the URL bar doesn't properly change to a permalink, and hitting 'refresh' or opening it in another browser deletes your history. </p>
+        <p>Many libraries use the OCLC PICA software, such as the <a href="http://opc4.kb.nl/">catalogue of the Koninklijke Bibliotheek</a> (Dutch National Library) or the <a href="http://bibliotheek.gahetna.nl">Dutch National Archive</a>. Unfortunately, it's not very user-friendly, and there's no easy way from the interface to generate a permalink to a record. When browsing search results the URL bar doesn't properly change to a permalink, and hitting 'refresh' or opening it in another browser deletes your history. </p>
 
         <p>However, with this small <a href="https://en.wikipedia.org/wiki/Bookmarklet">bookmarklet</a> you can add a small button to your browser that automatically generates a permalink from any book page.</p>
 
         <p>To use it, simply drag the button below to your bookmark bar, and press it whenever you are on a book record page in the catalogue.</p>
 
-        <a class="textbutton" href="javascript:var%20s%20%3D%20document.createElement(%27script%27)%3Bs.src%20%3D%20%27http%3A%2F%2Ftools.wmflabs.org%2Fhay%2Fkbpermalink%2Fkbpermalink.js%27%3Bdocument.body.appendChild(s)%3Bvoid(0)%3B">KB Permalink</a>
+        <a class="textbutton" href="javascript:var%20s%20%3D%20document.createElement(%27script%27)%3Bs.src%20%3D%20%27http%3A%2F%2Ftools.wmflabs.org%2Fhay%2Fkbpermalink%2Fkbpermalink.js%27%3Bdocument.body.appendChild(s)%3Bvoid(0)%3B">Pica Permalink</a>
 <?php
     Hay::footer();
 ?>
--- public_html/kbpermalink/kbpermalink.js
+++ public_html/kbpermalink/kbpermalink.js
@@ -1,4 +1,14 @@
-(function(d) {
+(function(d, loc) {
+    // First check if we're actually on a OCLC PICA site
+    var htmlAttrs = document.querySelector('html').attributes;
+
+    if ( (!htmlAttrs.length) ||
+         (htmlAttrs[0].nodeValue.indexOf("oclcpica") === -1)
+    ) {
+        alert("This is not an OCLC PICA catalogue...");
+        return;
+    }
+
     var links = document.querySelectorAll('.link_gen');
     var link = [].slice.call(links).filter(function(l) {
         return l.href.indexOf("PPN") !== -1 && l.href.indexOf('SAVEINFO') !== -1;
@@ -10,6 +20,7 @@
     }
 
     var ppn = link[0].href.match(/PPN=(\d\w*)/)[1];
-    var url = "http://opc4.kb.nl/PPN?PPN=" + ppn;
+    var baseUrl = loc.protocol + '//' + loc.hostname;
+    var url = baseUrl + "/PPN?PPN=" + ppn;
     window.prompt("Here's the permalink", url);
-})(document);
+})(document, window.location);
