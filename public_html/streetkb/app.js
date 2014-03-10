8555b3b72da66eca96c1c16aa48b972f2413490b
Hay Kranen <huskyr@gmail.com>
2014-03-10 13:49:28 +0100

    adding streetkb

---
+++ public_html/streetkb/app.js
@@ -0,0 +1,31 @@
+function App() {
+    this.pano = this.initPano();
+}
+
+App.prototype = {
+    getPano : function() {
+        return {
+            tiles : {
+                tileSize : new google.maps.Size(1024, 512),
+                worldSize : new google.maps.Size(1024, 512),
+                centerHeading : 105,
+                getTileUrl : this.getPanoTile.bind(this)
+            }
+        }
+    },
+
+    getPanoTile : function(pano) {
+        this.pano.setZoom(2);
+        return 'pano2.jpg';
+    },
+
+    initPano : function() {
+        var panoEl = $("#pano").get(0);
+
+        return new google.maps.StreetViewPanorama(panoEl, {
+            pano : 'pano1',
+            panoProvider : this.getPano.bind(this),
+            visible : true
+        });
+    },
+};
