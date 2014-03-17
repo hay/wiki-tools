c7bad457469e2754e6e99b86be9dbb289c1869d0
Hay Kranen <huskyr@gmail.com>
2014-03-17 10:47:08 +0100

    updated the overview page

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
 ?>
         <h1>Hay's tools</h1>
 
-        <p>Hello. I'm <a href="http://www.haykranen.nl">Hay Kranen</a> and this is a list of some tools i've made. I'm currently the <a href="https://en.wikipedia.org/wiki/Wikipedia:GLAM/KBNA">Wikipedian in Residence</a> for the Dutch <a href="http://www.gahetna.nl">National Archive</a> and the <a href="http://www.kb.nl">National Library</a>.</p>
+        <p>Hello. I'm <a href="http://www.haykranen.nl">Hay Kranen</a> and this is a list of some tools i've made. Until July 2014 i'm the <a href="https://en.wikipedia.org/wiki/Wikipedia:GLAM/KBNA">Wikipedian in Residence</a> for the Dutch <a href="http://www.gahetna.nl">National Archive</a> (Nationaal Archief) and the <a href="http://www.kb.nl">National Library</a> (Koninklijke Bibliotheek).</p>
 
         <p>If you want to get in contact send me an <a href="mailto:hay@wikimedia.nl">mail</a> or leave a message on my <a href="https://en.wikipedia.org/wiki/User_talk:Husky">Talk page</a>. I'm also on <a href="http://www.twitter.com/hayify">Twitter</a>.</p>
 
@@ -14,13 +14,16 @@
 
         <dl class="dl-horizontal">
             <dt><a href="kbpermalink/">Pica Permalink</a></dt>
-            <dd>A bookmarklet to generate a permalink for the catalogue of the Dutch National Library (Koninklijke Bibliotheek)</dd>
+            <dd>A bookmarklet to generate a permalink for the catalogue of the Dutch National Library (Koninklijke Bibliotheek) or any other Pica/OCLC catalogue</dd>
 
             <dt><a href="nadownload/">NA Download</a></dt>
             <dd>A small tool to download all images from a Dutch National Archive inventory</dd>
 
             <dt><a href="nasearch/">NA Search</a></dt>
             <dd>Bulk search the NA image archives</dd>
+
+            <dt><a href="natranscript/">NA Transcript</a></dt>
+            <dd>Download transcripts of archive pieces</dd>
 
             <dt><a href="streetwiki/">Streetwiki</a></dt>
             <dd>View Wikipedia articles in Google Maps &amp; Street View</dd>
