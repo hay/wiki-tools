a866f379b5ba657c3336f60db571082c751cca89
Hay Kranen <huskyr@gmail.com>
2014-04-24 00:32:03 +0200

    some fixes

--- public_html/wdview/app.js
+++ public_html/wdview/app.js
@@ -1,9 +1,9 @@
+// TODO: somehow fold this into the tool
+// https://www.wikidata.org/wiki/MediaWiki:Gadget-AuthorityControl.js
 var app = angular.module('app', []);
-var LANG = 'nl';
 
 app.factory('api', function($http, $sce) {
     var API_ENDPOINT = "http://api.haykranen.nl";
-    var lang = LANG;
 
     function call(cmd, params, cb) {
         var req = $http({
@@ -40,16 +40,14 @@ app.factory('api', function($http, $sce) {
 
         claim.valueHtml = $sce.trustAsHtml(claim.value);
 
-        console.log(claim);
-
         return claim;
     }
 
-    function wikidataSearch(q, cb) {
+    function wikidataSearch(q, lang, cb) {
         call('wikidata', { method : 'search', q : q, language : lang}, cb);
     }
 
-    function wikidataEntity(q, cb) {
+    function wikidataEntity(q, lang, cb) {
         call('wikidata', {
             method : 'entity',
             q : q,
@@ -67,17 +65,43 @@ app.factory('api', function($http, $sce) {
     };
 });
 
-app.controller('itemCtrl', function($scope, $http, api, $window) {
-    $scope.q = $window.WIKIDATA_Q;
+app.controller('itemCtrl', function($scope, $http, api, $window, $sce) {
+    $scope.id = $window.WIKIDATA_ID;
+    $scope.lang = $window.WIKIDATA_LANG || 'en';
+    $scope.loading = true;
 
-    if ($scope.q[0] !== "Q") {
-        $scope.q = "Q" + $scope.q;
+    if ($scope.id[0] !== "Q") {
+        $scope.id = "Q" + $scope.id;
     }
 
-    api.wikidataEntity($scope.q, function(data) {
+    api.wikidataEntity($scope.id, $scope.lang, function(data) {
+        $scope.loading = false;
         $scope.label = data.labels;
         $scope.id = data.id;
         $scope.description = data.descriptions;
         $scope.claims = data.claims;
+        $scope.image = data.claims.filter(function(claim) {
+            return claim.value.indexOf("<img") !== -1;
+        })[0].value;
+        $scope.image = $sce.trustAsHtml($scope.image);
     });
+});
+
+app.controller('searchResultsCtrl', function($scope, api, $window) {
+    $scope.q = $window.WIKIDATA_Q;
+    $scope.lang = $window.WIKIDATA_LANG || 'en';
+    $scope.loading = true;
+
+    api.wikidataSearch($scope.q, $scope.lang, function(data) {
+        $scope.loading = false;
+        $scope.results = data.response;
+    });
+});
+
+app.controller('appCtrl', function($scope) {
+    $scope.lang = 'en';
+
+    $scope.changeLang = function() {
+
+    };
 });
--- public_html/wdview/index.php
+++ public_html/wdview/index.php
@@ -4,56 +4,106 @@
     $hay = new Hay("wdview");
     $hay->header();
 ?>
-    <h1><?php $hay->title(); ?></h1>
+<style>
+    h1 {
+        margin: 0 0 20px 0;
+    }
 
-    <?php if (empty($_GET['q'])) : ?>
+    form {
+        margin-top: 5px;
+    }
+</style>
+
+<div ng-app="app">
+    <div class="row" ng-controller="appCtrl">
+        <div class="col-md-12">
+            <a href="#" ng-click="changeLang" class="pull-right">{{lang}}</a>
+        </div>
+
+        <div class="col-md-6">
+            <h1><a href="index.php"><?php $hay->title(); ?></a></h1>
+        </div>
+
+        <div class="col-md-6">
+            <form action="index.php" method="get" role="form">
+                <div class="input-group">
+                    <input type="text" id="q" name="q" class="form-control" autocomplete="off" />
+
+                    <div class="input-group-btn">
+                        <button type="submit" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-search"></span>
+                            Lookup
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php if (!empty($_GET['id'])): ?>
+        <script>
+            // This should be a lot prettier
+            var WIKIDATA_ID = "<?php echo $_GET['id']; ?>";
+        </script>
+
+        <div ng-controller="itemCtrl" class="row">
+            <div ng-show="loading">Loading...</div>
+
+            <div class="col-md-12">
+                <h1>{{label}}</h1>
+                <h2>{{description}}</h2>
+            </div>
+
+            <div class="col-md-6">
+                <div class="image" ng-bind-html="image">{{image}}</div>
+            </div>
+
+            <div class="col-md-6">
+                <table class="table">
+                    <tr>
+                        <td><strong>Wikidata ID</strong></td>
+                        <td>{{id}}</td>
+                    </tr>
+                    <tr ng-repeat="claim in claims">
+                        <td><strong>{{claim.property_labels}}</strong></td>
+                        <td ng-bind-html="claim.valueHtml">{{claim.valueHtml}}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    <?php elseif (!empty($_GET['q'])): ?>
+        <script>
+            var WIKIDATA_Q ="<?php echo $_GET['q']; ?>";
+        </script>
+
+        <div ng-controller="searchResultsCtrl">
+
+            <h3>Search results</h3>
+
+            <div ng-show="loading">Loading...</div>
+
+            <div id="searchresults">
+                <ul>
+                    <li ng-repeat="result in results">
+                        <a href="index.php?id={{result.id}}">
+                            {{result.label}} - {{result.description}}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    <?php else: ?>
         <p class="lead"><?php $hay->description(); ?></p>
 
-        <form action="index.php" method="get" role="form">
-            <div class="input-group">
-                <input type="text" id="q" name="q" class="form-control" autocomplete="off" />
-
-                <div class="input-group-btn">
-                    <button type="submit" class="btn btn-primary">
-                        <span class="glyphicon glyphicon-search"></span>
-                        Lookup
-                    </button>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="lang">Language</label>
-                <select name="lang" id="lang" class="form-control">
-                    <option value="nl">Nederlands</option>
-                    <option value="en">English</option>
-                </select>
-            </div>
-        </form>
+        <p>Simply use the search form at the top of this page to try it out.</p>
 
         <h3>Examples</h3>
         <ul>
             <li><a href="?q=690554">Aart Staartjes</a></li>
             <li><a href="?q=Q426489">Mies Bouwman</a></li>
         </ul>
-
-    <?php else: ?>
-        <script>
-            // This should be a lot prettier
-            var WIKIDATA_Q = "<?php echo $_GET['q']; ?>";
-        </script>
-
-        <div id="item" ng-app="app" ng-controller="itemCtrl">
-            <h1>{{label}} <small>({{id}})</small></h1>
-            <h2>{{description}}</h2>
-
-            <dl>
-                <div ng-repeat="claim in claims">
-                    <dt>{{claim.property_labels}}</dt>
-                    <dd ng-bind-html="claim.valueHtml">{{claim.valueHtml}}</dd>
-                </div>
-            </dl>
-        </div>
     <?php endif; ?>
+</div>
 
     <script src="../common/angular.js"></script>
     <script src="../common/jquery.js"></script>
@@ -66,7 +116,7 @@
             cb(['aap' + q]);
         }
 
-        $("#q").typeahead({
+        $("#qa").typeahead({
             minLength : 3
         }, {
             name : 'wdview',
