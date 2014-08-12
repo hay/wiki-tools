5da449fe034a9bab0693438692bf26704d92c261
Hay Kranen <huskyr@gmail.com>
2014-08-12 22:04:58 +0200

    Adding toolinfo.json to these files

---
+++ public_html/kbpermalink/toolinfo.js
@@ -0,0 +1,7 @@
+{
+    "title" : "Pica Permalink",
+    "description" : "A small Javascript bookmarklet to generate a permalink to library catalogues that uses the OCLC PICA software.",
+    "url" : "http://tools.wmflabs.org/hay/kbpermalink",
+    "keywords" : ["kb", "bookmarklet", "permalink", "libraries", "oclc", "pica"],
+    "author" : "Hay Kranen"
+}
---
+++ public_html/nasearch/toolinfo.js
@@ -0,0 +1,7 @@
+{
+    "title" : "NA search",
+    "description" : "A tool to do a mass search in the Dutch National Archive photo collection",
+    "url" : "http://tools.wmflabs.org/hay/nasearch",
+    "keywords" : ["search", "nationaal archief", "national archive", "photos", "glam"],
+    "author" : "Hay Kranen"
+}
