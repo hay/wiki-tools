abff5f0df0282932db0a2621309e3aad7c64a79f
Hay Kranen <huskyr@gmail.com>
2014-08-13 17:17:24 +0200

    No longer using an array here

--- public_html/kbpermalink/toolinfo.js
+++ public_html/kbpermalink/toolinfo.js
@@ -2,6 +2,6 @@
     "name" : "Pica Permalink",
     "description" : "A small Javascript bookmarklet to generate a permalink to library catalogues that uses the OCLC PICA software.",
     "url" : "http://tools.wmflabs.org/hay/kbpermalink",
-    "keywords" : ["kb", "bookmarklet", "permalink", "libraries", "oclc", "pica"],
+    "keywords" : "kb, bookmarklet, permalink, libraries, oclc, pica",
     "author" : "Hay Kranen"
 }
--- public_html/nasearch/toolinfo.js
+++ public_html/nasearch/toolinfo.js
@@ -2,6 +2,6 @@
     "name" : "NA search",
     "description" : "A tool to do a mass search in the Dutch National Archive photo collection",
     "url" : "http://tools.wmflabs.org/hay/nasearch",
-    "keywords" : ["search", "nationaal archief", "national archive", "photos", "glam"],
+    "keywords" : "search, nationaal archief, national archive, photos, glam",
     "author" : "Hay Kranen"
 }
