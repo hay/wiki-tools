0f4b5d6b17933085e532ec8b6c0ce9705f4371c3
Hay Kranen <huskyr@gmail.com>
2014-08-13 23:00:48 +0200

    Slashes should be there

--- lib/crawl-tools.php
+++ lib/crawl-tools.php
@@ -1,6 +1,6 @@
 <?php
-    require __DIR__ . 'vendor/autoload.php';
-    require __DIR__ . 'config.php';
+    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/config.php';
     require __DIR__ . '/../public_html/directory/lib/class-api.php';
     require __DIR__ . '/../public_html/directory/lib/class-crawl.php';
 
