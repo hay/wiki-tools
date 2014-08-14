30da701b55b5c2c8f2d219029af3da69eab7386a
Hay Kranen <huskyr@gmail.com>
2014-08-14 15:19:12 +0200

    Adding Piwik tracking code

--- lib/class-hay.php
+++ lib/class-hay.php
@@ -98,6 +98,22 @@ HTML;
 
     <script src="${root}/vendor/jquery/jquery.js"></script>
     <script src="${root}/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Piwik, not tracking any privacy-sensitive data -->
+    <script>
+      var _paq = _paq || [];
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.bykr.org/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 1]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+        g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="http://stats.bykr.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
 </body>
 </html>
 HTML;
