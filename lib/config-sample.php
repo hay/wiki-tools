0fe91368001853af3b474909ff774e147a9302fa
Hay Kranen <huskyr@gmail.com>
2014-08-18 14:55:04 +0200

    Removing piwik

--- lib/config-sample.php
+++ lib/config-sample.php
@@ -1,5 +1,5 @@
 <?php
-    define('ROOT', 'http://localhost/git/wiki-tools/public_html/');
+    define('ROOT', '//localhost/git/wiki-tools/public_html/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'root');
