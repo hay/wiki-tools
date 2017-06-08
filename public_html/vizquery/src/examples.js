f92158fa33e118e46c87a756f6eddb8522d4ca59
Hay Kranen <huskyr@gmail.com>
2017-06-09 01:13:53 +0200

    Adding some more examples

--- public_html/vizquery/src/examples.js
+++ public_html/vizquery/src/examples.js
@@ -1,6 +1,6 @@
 export default [
     {
-        "description" : "Painters born in Leiden",
+        "description" : "Painters born in Leiden, the Netherlands",
         "data" : [
             "P31 Q5",
             "P106 Q1028181",
@@ -8,10 +8,25 @@ export default [
         ]
     },
     {
+        "description" : "Train stations in the Czech Republic",
+        "data" : [
+            "P31 Q55488",
+            "P17 Q213"
+        ]
+    },
+    {
         "description" : "Municipalities in the province of Gelderland, the Netherlands",
         "data" : [
             "P31 Q2039348",
             "P131 Q775"
+        ]
+    },
+    {
+        "description" : "Female trumpet players",
+        "data" : [
+            "P31 Q5",
+            "P1303 Q8338",
+            "P21 Q6581072"
         ]
     },
     {
