92983d7ee11d859c3b1913619edb554274a6952a
Hay Kranen <huskyr@gmail.com>
2020-05-10 00:24:30 +0200

    Adding sdocquery and _template

---
+++ public_html/_template/js/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './components/app.vue';
+
+new Vue({
+    el : "#app",
+
+    components : { App }
+});
---
+++ public_html/sdocquery/js/components/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>hoi</h1>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
