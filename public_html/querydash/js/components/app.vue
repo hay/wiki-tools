284d7a572691489e0a9ca9d40f2c93c871aec592
Hay Kranen <hay@haykranen.nl>
2021-04-01 00:24:59 +0200

    Prototype seems to work

--- public_html/querydash/js/components/app.vue
+++ public_html/querydash/js/components/app.vue
@@ -27,9 +27,17 @@
                 class="button">
                 <span>🔗 Save to URL</span>
             </button>
+
+            <button
+                v-show="queries.length"
+                v-on:click="run"
+                class="button">
+                <span>▶️ Run queries</span>
+            </button>
         </menu>
 
-        <ul class="queries">
+        <ul class="queries"
+            v-bind:class="editMode ? 'queries--edit' : 'queries--view'">
             <li v-for="(query, index) in queries"
                 v-bind:key="index"
                 class="queries__item">
@@ -45,7 +53,8 @@
                     placeholder="Query title"
                     v-model="query.name" />
 
-                <p v-show="!editMode">
+                <p class="queries__name"
+                    v-show="!editMode">
                     {{query.name}}
                 </p>
 
@@ -57,19 +66,31 @@
                 <div
                     v-show="!editMode"
                     class="queries__value">
-                    {{query.value}}
+                    <span
+                        class="queries__bar"
+                        v-bind:style="getStyle(query.value)"></span>
+                    <strong>{{query.value}}</strong>
                 </div>
             </li>
         </ul>
+
+        <p class="dashboard__timestamp">
+            Timestamp of this run: {{timestamp}}
+        </p>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import Vue from 'vue';
+
     export default {
         data() {
             return {
                 editMode : false,
-                queries : []
+                largestValue : 1,
+                queries : [],
+                timestamp : null
             };
         },
 
@@ -85,29 +106,80 @@
                 this.editMode = true;
             },
 
+            getStyle(val) {
+                const width = Math.round((val / this.largestValue) * 100);
+                return { width : `${width}%`};
+            },
+
             loadState() {
                 if (!window.location.hash) return;
                 const hash = window.location.hash.slice(1);
-                let queries;
 
                 try {
-                    queries = JSON.parse(window.atob(hash));
+                    const data = JSON.parse(window.atob(hash));
+
+                    if (!data.queries || !data.timestamp) {
+                        throw new Error('Invalid hash data');
+                    }
+
+                    this.queries = data.queries;
+                    this.timestamp = data.timestamp;
                 } catch (e) {
                     console.error('URL hash decoding error!');
                     return;
                 }
 
-                this.queries = queries;
+                // Set largest value
+                this.queries.forEach((q) => {
+                    if (q.value > this.largestValue) {
+                        this.largestValue = Number(q.value);
+                    }
+                });
             },
 
             removeQuery(index) {
                 this.queries = this.queries.filter((q, qIndex) => qIndex !== index);
             },
 
+            async run() {
+                // Make sure the user knows we're busy
+                this.queries.forEach(i => i.value = 'Loading...');
+                await Vue.nextTick();
+
+                for (const item of this.queries) {
+                    let query = `
+                        select (count(?item) as ?count) where {
+                          ${item.query}
+                        }
+                    `.trim();
+
+                    try {
+                        const res = await axios.get('https://query.wikidata.org/sparql', {
+                            params : { query }
+                        });
+
+                        const val = Number(res.data.results.bindings[0].count.value);
+                        item.value = val;
+
+                        if (val > this.largestValue) {
+                            this.largestValue = val;
+                        }
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+
+                this.saveState();
+            },
+
             saveState() {
                 // This is pretty dirty, but oh well
-                const state = window.btoa(JSON.stringify(this.queries));
-                window.location.hash = state;
+                const state = {
+                    queries : this.queries,
+                    timestamp : new Date().toISOString()
+                };
+
+                window.location.hash = window.btoa(JSON.stringify(state));
             }
         },
 
