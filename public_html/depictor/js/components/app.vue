1cbbf270047656a462f95047cfb6aaa7b9922371
Hay Kranen <hay@haykranen.nl>
2021-08-24 12:59:35 +0200

    Fixed loading screen

--- public_html/depictor/js/components/app.vue
+++ public_html/depictor/js/components/app.vue
@@ -29,7 +29,7 @@
             },
 
             isLoading() {
-                return this.$store.state.isLoading;
+                return this.$store.state.loading;
             },
 
             message() {
