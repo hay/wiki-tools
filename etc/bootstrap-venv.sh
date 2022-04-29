04e29cc4ef140b9b6f0df525333b522472ae068d
Hay Kranen <hay@haykranen.nl>
2022-04-29 11:09:35 +0200

    Adding new toolforge jobs stuff

---
+++ etc/bootstrap-venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create the venv
+python3 -m venv pyvenv
+
+# activate it
+source pyvenv/bin/activate
+
+# upgrade pip inside the venv and add support for the wheel package format
+pip install -U pip wheel
+
+# install some concrete packages
+pip install requests
+pip install jinja2
