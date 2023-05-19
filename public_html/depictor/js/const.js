edc43de90d2d9eae08b7040d4009de18427b2cfd
Hay Kranen <hay@haykranen.nl>
2023-05-19 18:14:52 +0300

    Changed defaults for thumb/item to things recommended by Timo, at least partly fixes #93

--- public_html/depictor/js/const.js
+++ public_html/depictor/js/const.js
@@ -1,6 +1,6 @@
 export const COMMONS_USER_PREFIX = 'https://commons.wikimedia.org/wiki/User:';
 export const DEFAULT_LOCALE = 'en';
-export const IMAGE_SIZE = 600;
+export const IMAGE_SIZE = 640;
 export const LOCAL_API_ENDPOINT = './api/index.php';
 export const MAX_API_TRIES = 3;
 export const MAX_API_CHECK_TRIES = 100;
@@ -11,4 +11,4 @@ export const MIN_CANDIDATES_FOR_CHALLENGE = 100;
 export const MIN_CHALLENGE_TITLE_LENGTH = 5;
 export const MIN_CHALLENGE_SHORTDESCRIPTION_LENGTH = 25;
 export const MIN_ITEMS_FOR_CHALLENGE = 10;
-export const THUMB_SIZE = 300;
+export const THUMB_SIZE = 320;
