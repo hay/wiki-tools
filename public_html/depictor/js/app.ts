8aef62ffe888d05487fff159d1a668e5ba94c279
Bruno Perel <bruno.perel@firstvet.com>
2026-03-13 21:20:11 +0100

    Cleanup

--- public_html/depictor/js/app.ts
+++ public_html/depictor/js/app.ts
@@ -1,19 +1,21 @@
-import '../scss/style.scss';
-import { $ } from 'donot';
-import { getJson } from './util';
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-import Vuex from 'vuex';
-import App from './components/app.vue';
-import WmButton from './components/wm-button.vue';
-import createStore from './store';
-import { test } from './test';
+import "../scss/style.scss";
+import { $ } from "donot";
+import { getJson } from "./util";
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import Vuex from "vuex";
+import App from "./components/app.vue";
+import WmButton from "./components/wm-button.vue";
+import createStore from "./store";
+import { test } from "./test";
 
 async function createApp() {
     Vue.use(Vuex);
     Vue.use(VueI18n);
 
-    const locales = await getJson('./locales.json') as { messages?: Record<string, Record<string, string>> };
+    const locales = await getJson("./locales.json") as {
+        messages: Record<string, Record<string, string>>;
+    };
     const ctx = window.__ctx__;
 
     if (ctx?.isDebug) {
@@ -21,48 +23,50 @@ async function createApp() {
     }
 
     const storeOptions = Object.assign({}, window.__ctx__, {
-        locales : locales
+        locales,
     });
     const store = createStore(storeOptions);
 
     const i18n = new VueI18n({
         fallbackLocale: store.state.defaultLocale,
-        locale : store.state.locale,
-        messages : store.state.locales?.messages ?? {},
-        silentTranslationWarn : !store.state.isDebug
+        locale: store.state.locale,
+        messages: store.state.locales?.messages ?? {},
+        silentTranslationWarn: !store.state.isDebug,
     });
 
-    Vue.component('wm-button', WmButton);
+    Vue.component("wm-button", WmButton);
 
-    Vue.config.errorHandler = function(err: Error) {
+    Vue.config.errorHandler = function (err: Error) {
         console.error(err);
     };
 
     new Vue({
-        el : "#app",
+        el: "#app",
 
-        components : { App },
+        components: { App },
 
-        computed : {
+        computed: {
             screen() {
-                return (this as unknown as { $store: typeof store }).$store.state.screen;
-            }
+                return (this as unknown as { $store: typeof store }).$store
+                    .state.screen;
+            },
         },
 
-        i18n : i18n,
+        i18n: i18n,
 
-        methods : {
+        methods: {
             parseSearch() {
                 const url = new window.URL(window.location.href);
-                const $store = (this as unknown as { $store: typeof store }).$store;
+                const $store =
+                    (this as unknown as { $store: typeof store }).$store;
 
                 if (
                     url.searchParams.has("queryType") &&
                     url.searchParams.has("queryValue")
                 ) {
-                    $store.dispatch('query', {
-                        type : url.searchParams.get('queryType')!,
-                        value : url.searchParams.get('queryValue')!
+                    $store.dispatch("query", {
+                        type: url.searchParams.get("queryType")!,
+                        value: url.searchParams.get("queryValue")!,
                     });
                 }
 
@@ -76,7 +80,7 @@ async function createApp() {
                 if (url.searchParams.has("test")) {
                     test();
                 }
-            }
+            },
         },
 
         mounted() {
@@ -85,26 +89,26 @@ async function createApp() {
 
         store,
 
-        watch : {
+        watch: {
             screen(screen: string) {
                 const wrapper = $("#wrapper");
                 if (wrapper) {
-                    if (screen === 'game') {
-                        wrapper.setAttribute('is-fullscreen', '');
+                    if (screen === "game") {
+                        wrapper.setAttribute("is-fullscreen", "");
                     } else {
-                        wrapper.removeAttribute('is-fullscreen');
+                        wrapper.removeAttribute("is-fullscreen");
                     }
                 }
-            }
-        }
+            },
+        },
     } as any);
 }
 
 function addTouchClasses() {
-    const inputDevice = 'ontouchend' in window ? 'touch' : 'mouse';
-    const html = $('html');
+    const inputDevice = "ontouchend" in window ? "touch" : "mouse";
+    const html = $("html");
     if (html) {
-        html.classList.add('has-' + inputDevice);
+        html.classList.add("has-" + inputDevice);
     }
 }
 
