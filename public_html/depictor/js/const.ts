99c5cc20791c6b097f4687e9d566354099e8505b
Hay Kranen <hay@haykranen.nl>
2026-03-17 22:55:22 +0100

    #191 Update image size constants for consistency with MediaWiki standards

--- public_html/depictor/js/const.ts
+++ public_html/depictor/js/const.ts
@@ -1,7 +1,7 @@
 export const API_USER_AGENT:string = 'Depictor/1.0 (https://hay.toolforge.org/depictor/; hay@haykranen.nl)';
 export const COMMONS_USER_PREFIX:string = 'https://commons.wikimedia.org/wiki/User:';
 export const DEFAULT_LOCALE = 'en';
-export const IMAGE_SIZE = 640;
+export const IMAGE_SIZE = 500; // https://www.mediawiki.org/wiki/Common_thumbnail_sizes
 export const LOCAL_API_ENDPOINT = './api/index.php';
 export const MAX_API_TRIES = 3;
 export const MAX_API_CHECK_TRIES = 100;
@@ -13,4 +13,4 @@ export const MIN_CANDIDATES_FOR_CHALLENGE = 100;
 export const MIN_CHALLENGE_TITLE_LENGTH = 5;
 export const MIN_CHALLENGE_SHORTDESCRIPTION_LENGTH = 25;
 export const MIN_ITEMS_FOR_CHALLENGE = 10;
-export const THUMB_SIZE = 320;
+export const THUMB_SIZE = 330; // https://www.mediawiki.org/wiki/Common_thumbnail_sizes
