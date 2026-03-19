9aa954d2721a1589e58876957c20582e23c241f6
Hay Kranen <hay@haykranen.nl>
2026-03-19 23:05:10 +0100

    Updating config sample

--- lib/config-sample.php
+++ lib/config-sample.php
@@ -1,24 +1,20 @@
 <?php
-    define('ROOT', '//localhost/git/wiki-tools/public_html/');
+    define('ROOT', '//localhost:4080');
+    define('PUBLIC_HTML_PATH', 'html');
     define('PATH', dirname(__FILE__) . '/');
-    define('DB_HOST', 'mysql');
+    define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'root');
     define('DB_TOOLSDIRECTORY', 'toolsdirectory');
-    define('DEBUG', false);
     define('DB_TOOL_DATABASE', 'wikitools');
     define('TBL_DEPICTOR_CHALLENGES', 'depictor_challenges');
     define('TBL_DEPICTOR_FILES', 'depictor_files');
     define('TBL_DEPICTOR_ITEMS', 'depictor_items');
-    define('ENV_MODE', 'test'); // Set this to 'test' to display a message indicating that we're on the test server
-
-    // This is used for Depictor
+    define('DEBUG', false);
+    define('ENV_MODE', 'test');
     define('OAUTH_DEPICTOR', [
-        "consumer_key" => "xxx",
+        "consumer_key" => "xxxx",
         "consumer_secret" => "xxx"
     ]);
-
-    // These two should probably be the same, except if you want to
-    // use a different server for authentication
     define('OAUTH_ENDPOINT', "https://commons.wikimedia.org");
     define('COMMONS_ENDPOINT', "https://commons.wikimedia.org");
